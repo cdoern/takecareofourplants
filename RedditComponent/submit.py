03711b6e4d824450484bcbec6b48eb2937790f2d
Charlie Doern <cbdoer23@g.holycross.edu>
2021-03-02 22:21:57 -0500

    Update submit.py

--- RedditComponent/submit.py
+++ RedditComponent/submit.py
@@ -9,11 +9,11 @@ import numpy as np
 import random
 
 #praw setup
-reddit = praw.Reddit(client_id="GROOQTDDB22yjw",
-                     client_secret="I7r3qT0jLZN90KDu6ltGGDimFEg",
-                     user_agent="plantbot",
-                     username="takecareofourplants",
-                     password="seadawg01")
+reddit = praw.Reddit(client_id="",
+                     client_secret="",
+                     user_agent="",
+                     username="",
+                     password="")
 
 subreddit = reddit.subreddit('takecareofourplants')
 
